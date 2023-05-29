590adc0fcd60206c79670e34e1bc123aa2c44e59
mmoragu <58309060+mmoragu@users.noreply.github.com>
2023-05-29 17:06:53 +0200

    fix main, predicition

--- EPD2/Ejercicios-1-4/main.py
+++ EPD2/Ejercicios-1-4/main.py
@@ -30,11 +30,11 @@ if __name__ == '__main__':
 
 
     # Predict values for population sizes of 35, 000 and 70, 000
-    x_35= np.array([[0,35000/10000]])
-    x_70= np.array([0,70000/10000])
+    x_35= np.array([[1,35000/10000]])
+    x_70= np.array([1,70000/10000])
 
-    prediction_35= (np.dot(x_35,theta)[0] )*10000
-    prediction_70=( np.dot(x_70,theta)[0])*10000
+    prediction_35= (np.dot(x_35,theta) )*10000
+    prediction_70=( np.dot(x_70,theta))*10000
 
 
     print("Fin")
